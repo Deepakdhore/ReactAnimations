6c33542a29e52efc725d9f34139c38040223096c
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2025-09-02 12:48:10 +0530

    many things got added

--- AnimationsCard/vite-project/src/App.jsx
+++ AnimationsCard/vite-project/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { motion } from 'framer-motion'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(4)
-
+export default function App() {
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <motion.div classname="box">
+      <motion.div className="card"
+      drag
+      whileDrag={{
+        scale:[2,1,2,1,2]
+      }}
+      dragConstraints={{
+        top:-100,
+        left:0,
+        right:100,
+        bottom:100
+      }}
+      initial={{
+        x:0,
+        y:0
+      }}
+      // animate={{
+      //   x:[ 0  , 1000,1000, 0],
+      //   y:[-100,-100, 0 , 0],
+      //   rotate:[90,180,270,360],
+      //   scale:1,
+        
+      // }}
+      whileHover={{
+        backgroundColor:"blue"
+      }}
+      whileTap={{
+        rotate:3600
+      }}
+      transition={{
+        duration:3,
+       delay:1,
+        repeat:Infinity,
+        
+      }}
+      >
+      
+      </motion.div>
+     </motion.div>
     </>
   )
 }
 
-export default App
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ReactAnimations
+this is a testing repo for shared contribution on react animations
